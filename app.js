2aef1f28bbff8b3450028422291f8778ffe87e7d
Anthony <2240003@ipisa.edu.do>
2026-02-09 01:44:55 -0400

    Tercer commit: En este nuevo commit se modifico el app.js y el formulario.html, en el app.js se añadio una constante llamada post, datos y res (Response), en post se definio la ruta http:localhost:3000, en datos se guardaran los datos que el usuario ingrese en el formulario.htm mediante los inputs, para luego imprimirlos en la consola. La constante res se utiliza para guardar los datos en un archivo JSON, para luego poder compartirlo mediante una ruta, todo eso esta dentro de un try, el cual contiene esa constante, un if else (que notifica al usuario si algo salio mal en el proceso de guardar los datos segun se notifique en la carpeta routes en el backend, la cual tiene una consulta para saberlo), y ya por ultimo tiene un catch (el cual se encarga de detectar si hay algun error en el pase de datos, si lo detecta registrara el error en la consola).

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 document.getElementById('formContacto').addEventListener('submit', async (e) => {
     e.preventDefault();
-} )
+    const post= "http://localhost:3000";
+    const datos = {
+        nombre: document.getElementById('nombre').value,
+        contacto: document.getElementById('contacto').value,
+        mensaje: document.getElementById('mensaje').value
+    };
+    console.log(datos);
+
+    try {
+        const res = await fetch('http://localhost:3000/api/contacto', {
+            method: ("post"),
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(datos)
+        });
+        if (res.ok) {
+            alert("Guardado exitosamente");
+        } else {
+            alert("Error");
+        };
+
+    } catch (error) {
+        console.log(`Tremendo error: ${error}`);
+        throw (error);
+    };
+});
